8d60e2a7719436305ac83a823d08f1db86aec59e
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-05-23 14:49:48 -0700

    Completed guided project

--- Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
@@ -7,16 +7,108 @@ import Foundation
 //:
 //: The below example shows a protocol that requires conforming types have a particular property defined.
 
+// Getter - Read access
+// Setter - Write access
+
+protocol FullyNamed {
+    var fullName: String { get }
+}
+
+// 1. Adoption
+// 2. Conformance
+
+struct Person: FullyNamed {
+    var fullName: String
+}
+
+let johnny = Person(fullName: "Johnny Hicks")
+
+print(johnny.fullName)
+
+class StarShip: FullyNamed {
+    var prefix: String?
+    var name: String
+    
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+    
+    // Computed Property (Calculated Property)
+    var fullName: String {
+        // Ternary Operator
+        return (prefix != nil ? prefix! + " " : "") + self.name
+    }
+}
+
+var ncc1701 = StarShip(name: "Enterprise", prefix: "USS")
+ncc1701.fullName
+
+var firefly = StarShip(name: "Serenity")
+firefly.fullName
 
 
 //: Protocols can also require that conforming types implement certain methods.
 
+protocol GeneratesRandomNumbers {
+    func random() -> Int
+}
+
+class OneThroughTen: GeneratesRandomNumbers {
+    func random() -> Int {
+        return Int.random(in: 1...10)
+    }
+}
+
+let rand = OneThroughTen()
+rand.random()
 
 
 //: Using built-in Protocols
 
+extension StarShip: Equatable {
+    // "==" is the name of a built-in function
+    static func == (lhs: StarShip, rhs: StarShip) -> Bool {
+        if lhs.fullName == rhs.fullName { return true }
+        else { return false }
+    }
+}
+
+if ncc1701 == firefly {
+    print("Same Starship!")
+}
+
+// static refers to a class-level property (not an instance-level property)
 
 
 //: ## Protocols as Types
 
+class Dice {
+    let sides: Int
+    let generator: GeneratesRandomNumbers // can be anything that conforms to the GeneratesRandomNumbers protocol
+    
+    init(sides: Int, generator: GeneratesRandomNumbers) {
+        self.sides = sides
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() % sides) + 1
+        // Dice - 6 sides
+        // (1 % 6) + 1 = 6
+        // (2 % 6) + 1 = 5
+        // (3 % 6) + 1 = 4
+        // (4 % 6) + 1 = 3
+        // (5 % 6) + 1 = 2
+        // (6 % 6) + 1 = 1
+        // (7 % 6) + 1 = 2
+        // (8 % 6) + 1 = 3
+        // (9 % 6) + 1 = 4
+        // (10 % 6) + 1 = 5
+    }
+}
 
+var d6 = Dice(sides: 6, generator: OneThroughTen())
+for _ in 1...5 {
+    print("Random dice roll is \(d6.roll())")
+}
